15712e3fa6f09a7af56e79e2ed7bc3090395089f
Messorah <garabrielra@gmail.com>
2025-06-28 18:31:55 -0300

    add webdriving settings

---
+++ utils/browser.py
@@ -0,0 +1,21 @@
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def get_chrome_driver():
+    options = Options()
+
+    # if os.environ.get('FAST_FUNCTIONAL_TESTS') == '1':
+    #     options.add_argument('--headless')
+    #     options.add_argument('--disable-gpu')
+    #     options.add_argument('--no-sandbox')
+    #     options.add_argument('--disable-dev-shm-usage')
+
+    return webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
