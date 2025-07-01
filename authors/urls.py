747d0460c900b554aaa9ff303dc179fdff8082d7
Messorah <garabrielra@gmail.com>
2025-07-01 09:34:54 -0300

    making the form create a user

---
+++ authors/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from authors import views
+
+app_name = 'authors'
+
+urlpatterns = [
+    path('signup/', views.create_author, name="signup"),
+]
--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,14 @@ from authors.forms import CustomSignupForm
 
 
 def create_author(request):
-    form = CustomSignupForm()
+    if request.method == 'POST':
+        form = CustomSignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = CustomSignupForm()
 
     return render(request, 'authors/pages/authors.html', context={
         'form': form
