67bb2de48eb3a6dd89f1e3b098f8e197aafd3fc8
Messorah <garabrielra@gmail.com>
2025-10-01 18:06:01 -0300

    add validatePasswordHasSymbols and tests

--- authors/static/authors/js/validateForms.js
+++ authors/static/authors/js/validateForms.js
@@ -1,5 +1,6 @@
 export function validateUsernameLength(username) {
     if(username.length <= 3) {
+        // colocar tradução
         return 'Please enter at least 4 characters.';
     }
     
@@ -7,12 +8,37 @@ export function validateUsernameLength(username) {
     
 }
 
+// validade_username_already_exists
+// validate_email
+
+export function validatePasswordHasSymbols(password) {
+    if(password.length <= 0) return true;
+    
+    // password do not have symbols
+    if(/\W/.test(password) === false) {
+        // colocar tradução
+        return 'The password must contain symbols.';
+    }
+
+    return true;
+}
+
+function attachPasswordListener(passwordInput, form, errorSpan) {
+    passwordInput.addEventListener('input', () => {
+        const validation = validatePasswordHasSymbols(passwordInput.value);
+
+        if(validation !== true) {
+            errorSpan.textContent = validation;
+        } else {
+            errorSpan.textContent = '';
+        }
+
+    })
+}
 
 function attachUsernameListener(usernameInput, form, errorSpan) {
     usernameInput.addEventListener('input', () => {
         const validation = validateUsernameLength(usernameInput.value);
-
-        form.appendChild(errorSpan);
 
         if(validation !== true) {
             errorSpan.textContent = validation;
@@ -26,22 +52,26 @@ function attachUsernameListener(usernameInput, form, errorSpan) {
 function getElements() {
     const form = document.querySelector('.author-form');
     const usernameInput = form.querySelector('#id_username');
+    const passwordInput = form.querySelector('#id_password1');
 
-    return {form, usernameInput};
+    return {form, usernameInput, passwordInput};
 }
 
 
-function createErrorSpan() {
+function createErrorSpan(form) {
     const errorSpan = document.createElement('span');
     errorSpan.classList.add('error-span');
+
+    form.appendChild(errorSpan);
 
     return errorSpan;
 }
 
 
 export function main() {
-    const {form, usernameInput} = getElements();
-    const errorSpan = createErrorSpan();
+    const {form, usernameInput, passwordInput} = getElements();
+    const errorSpan = createErrorSpan(form);
 
     attachUsernameListener(usernameInput, form, errorSpan);
+    attachPasswordListener(passwordInput, form, errorSpan);
 }
--- frontend/__tests__/unit/FormValidator.test.js
+++ frontend/__tests__/unit/FormValidator.test.js
@@ -2,45 +2,64 @@
  * @jest-environment jsdom
  */
 
-import { main, validateUsernameLength } from '@js/validateForms';
+import { main, validatePasswordHasSymbols, validateUsernameLength } from '@js/validateForms';
 
-describe('Test input form validations', () => {
-    const formClass = 'author-form';
-    const usernameInputID = 'id_username';
-    const errorSpanClass = 'error-span';
 
-    let form;
-    let usernameInput;
+const FORM_CLASS = 'author-form';
+const USERNAME_INPUT_ID = 'id_username';
+const PASSWORD1_INPUT_ID = 'id_password1';
+const ERROR_SPAN_CLASS = 'error-span';
 
-    beforeEach(() => {
-        document.body.innerHTML = `
+
+function setupFormTest() {
+    document.body.innerHTML = `
         <form class="author-form">
-            <input type="text" id="${usernameInputID}" placeholder="Ex: Gabriel Rodrigues">
+            <input type="text" id="${USERNAME_INPUT_ID}" placeholder="Ex: Gabriel Rodrigues">
             <input type="email" id="id_email" placeholder="Ex: gabrielrodrigues@example.com">
             <input type="password" id="id_password1" placeholder="Ex 23#$1fsgKDL!">
             <input type="password" id="id_password2" placeholder="Repeat your password">
             <button type="submit">Submit</button>
         </form>
-        `;
+    `;
 
-        form = document.querySelector(`.${formClass}`);
-        usernameInput = form.querySelector(`#${usernameInputID}`);
+    const form = document.querySelector(`.${FORM_CLASS}`);
+    const usernameInput = form.querySelector(`#${USERNAME_INPUT_ID}`);
+    const password1Input = form.querySelector(`#${PASSWORD1_INPUT_ID}`);
 
-        main();
+    main();
+
+    return {
+        usernameInput, password1Input
+    };
+}
+
+function getErrorSpan() {
+    return document.querySelector(`.${ERROR_SPAN_CLASS}`);
+}
+
+
+describe('Test Username input form validations', () => {
+    let usernameInput;
+
+    beforeEach(() => {
+        ({ usernameInput } = setupFormTest());
     });
 
-    test('username length validator message', () => {
+    test('username length validator message success', () => {
+        expect(validateUsernameLength('abcd')).toBe(true);
+    });
+
+    test('username length validator message error', () => {
         expect(validateUsernameLength('abc')).toBe(
             'Please enter at least 4 characters.'
         );
-        expect(validateUsernameLength('abcd')).toBe(true);
     });
 
     test('displays error message when username is too short', () => {
         usernameInput.value = 'abc';
         usernameInput.dispatchEvent(new Event('input'));
 
-        const errorSpan = document.querySelector(`.${errorSpanClass}`);
+        const errorSpan = getErrorSpan();
         expect(errorSpan).not.toBeNull();
         expect(errorSpan.textContent).toBe('Please enter at least 4 characters.');
     })
@@ -49,8 +68,45 @@ describe('Test input form validations', () => {
         usernameInput.value = 'abcd';
         usernameInput.dispatchEvent(new Event('input'));
 
-        const errorSpan = document.querySelector(`.${errorSpanClass}`);
+        const errorSpan = getErrorSpan();
         expect(errorSpan).not.toBeNull();
         expect(errorSpan.textContent).toBe('');
     })
 });
+
+
+describe('Test Password input form validations', () => {
+    let password1Input;
+
+    beforeEach(() => {
+        ({ password1Input } = setupFormTest());
+    });
+
+    test('password symbols validator message success', () => {
+        expect(validatePasswordHasSymbols('ab12!')).toBe(true);
+    });
+
+    test('password symbols validator message error', () => {
+        expect(validatePasswordHasSymbols('ab12')).toBe(
+            'The password must contain symbols.'
+        );
+    });
+
+    test('password no has symbols show error', () => {
+        password1Input.value = '123456ab';
+        password1Input.dispatchEvent(new Event('input'));
+
+        const errorSpan = getErrorSpan();
+        expect(errorSpan).not.toBeNull();
+        expect(errorSpan.textContent).toBe('The password must contain symbols.');
+    });
+
+    test('password has symbols do not show error', () => {
+        password1Input.value = '123456a!';
+        password1Input.dispatchEvent(new Event('input'));
+
+        const errorSpan = getErrorSpan();
+        expect(errorSpan).not.toBeNull();
+        expect(errorSpan.textContent).toBe('');
+    });
+})
