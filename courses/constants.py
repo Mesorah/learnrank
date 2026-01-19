ac5d8ea14f92b85c0c08fd7b2fb7348a16d9692c
Mesorah <gabrielcambara123@gmail.com>
2026-01-19 19:16:42 -0300

    Add constants and refactor the html

---
+++ courses/constants.py
@@ -0,0 +1,24 @@
+from django.utils.translation import gettext_lazy as _lazy
+
+# Errors
+
+
+# Placeholders
+
+
+# Labels
+
+
+# SUCCESS MESSAGES
+
+# TITLES
+TITLE_INDEX = _lazy('Courses')
+
+
+# VIEWS
+# FORM ACTIONS
+
+# TEMPLATES
+
+
+# TITLES KEYS
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+import courses.constants as const
+
 
 def index(request):
-    return render(request, 'courses/pages/index.html')
+    return render(request, 'courses/pages/index.html', context={
+        'title': const.TITLE_INDEX
+    })
